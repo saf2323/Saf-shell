5de8d0019991ad0f9ece980e74c6a485262ca4e5
Saf <122736181+saf2323@users.noreply.github.com>
2023-06-02 13:09:39 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+import os
+
+os.system('pip install matplotlib')
+os.system('pip install colorthief')
+os.system('pip install pyfiglet')
+os.system('pip install termcolor')
+os.system('pip install qrcode')
+os.system('pip install click_shell')
+os.system('pip install pyttsx3')
